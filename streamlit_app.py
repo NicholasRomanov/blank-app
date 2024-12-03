11d3ce8564ed25a8a10a41da1ea12be588cdf181
NicholasRomanov <103803109+NicholasRomanov@users.noreply.github.com>
2024-12-03 19:14:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,4 +33,5 @@ st.sidebar.header("Your Order")
 if st.session_state.order:
     for ordered_item, quantity in st.session_state.order.items():
         st.sidebar.write(f"{ordered_item} {quantity}x")
- ⬤
+else:
+    st.sidebar.write("Your order is empty.")
